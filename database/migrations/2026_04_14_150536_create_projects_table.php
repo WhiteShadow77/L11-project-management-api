f6716ed86eef5bd08bcdee1a6014e782f123fc2d
alex <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>
2026-04-27 18:47:58 +0300

    Improved

--- database/migrations/2026_04_14_150536_create_projects_table.php
+++ database/migrations/2026_04_14_150536_create_projects_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
             $table->string('title');
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->timestamps();
         });
     }
